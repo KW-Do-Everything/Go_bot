49265b6b0c3c4611d4037b5ae9fd7811dc18ec8a
yun-goon <sirius584@naver.com>
2024-04-29 19:57:54 +0900

    4 29

--- install/baduk_vision/lib/python3.10/site-packages/baduk_vision/VisionModule/__init__.py
+++ install/baduk_vision/lib/python3.10/site-packages/baduk_vision/VisionModule/__init__.py
@@ -1,4 +1,4 @@
 from .preprocessing import *
 from .initialize import *
 from .utils import *
-from .letnetgo import *
+from .lenetgo import *
--- install/baduk_vision/share/baduk_vision/launch/baduk_vision_launch.py
+++ install/baduk_vision/share/baduk_vision/launch/baduk_vision_launch.py
@@ -1,16 +1,13 @@
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess
 
-        
 """ExecuteProcess(
-    cmd=["ros2", "run", "v4l2_camera", "v4l2_camera_node", "--ros-args", "-p", "image_size:=[1280,960]"], output="screen"
-),"""
+            cmd=["ros2", "run", "usb_cam", "usb_cam_node_exe"], output="screen"
+        ),"""
+                
 def generate_launch_description():
     
     return LaunchDescription([
-        ExecuteProcess(
-            cmd=["ros2", "run", "usb_cam", "usb_cam_node_exe"], output="screen"
-        ),
         
         ExecuteProcess(
             cmd=["ros2", "run", "baduk_vision", "server_listener"], output="screen"
